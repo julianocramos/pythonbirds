38bc1d4ed5120cc4804cb4aa2d559f8acb384d20
julianocramos <juliano_ nj@hotmail.com>
2020-08-28 17:32:23 -0300

    Criado e removido atributo do tipo pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,25 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=33):
+    def __init__(self, *filhos, nome=None, idade=33):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá{id(self)}'
 
+
 if __name__ == '__main__':
-    p = Pessoa('Renzo')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Juliano'
-    print(p.nome)
-    print(p.idade)
+    juliano = Pessoa(nome='Juliano')
+    renzo = Pessoa(juliano, nome='Renzo')
+    print(Pessoa.cumprimentar(renzo))
+    print(id(renzo))
+    print(renzo.cumprimentar())
+    print(renzo.nome)
+    print(renzo.idade)
+    for filho in renzo.filhos:
+        print(filho.nome)
+    renzo.sobrenome = 'Lemos'
+    print(renzo.sobrenome)
+    del renzo.filhos
+    print(juliano.__dict__)
+    print(renzo.__dict__)
