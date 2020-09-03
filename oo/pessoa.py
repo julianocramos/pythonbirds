654c587a3660c6f04651e4a1c7fc17a4ff641391
julianocramos <juliano_ nj@hotmail.com>
2020-09-03 12:43:13 -0300

    Implementado class direcao e motor

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,13 +1,21 @@
 class Pessoa:
     olhos = 2
 
-    def __init__(self, *filhos, nome=None, idade=33):
+    def __init__(self, *filhos, nome=None, idade=None):
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá{id(self)}'
+
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributo_de_classes(cls):
+        return f'{cls} - olhos {cls.olhos}'
 
 
 if __name__ == '__main__':
@@ -30,3 +38,6 @@ if __name__ == '__main__':
     print(Pessoa.olhos)
     print(renzo.olhos)
     print(juliano.olhos)
+    print(id(Pessoa.olhos), id(juliano.olhos), id(renzo.olhos))
+    print(Pessoa.metodo_estatico(), juliano.metodo_estatico())
+    print(Pessoa.nome_e_atributo_de_classes(), juliano.nome_e_atributo_de_classes())
